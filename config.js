f3f8974bd61f8a99d78ed0f44affd266ce872a83
Divyanshu Agrawal <divyanshu123@hotmail.com>
2019-07-05 01:26:46 +0530

    Fix CORS issues for gh-pages

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
     secret: "I love BITS Pilani",
-    allowedOrigins: ["http://localhost:3001", "http://127.0.0.1:5500"],
+    allowedOrigins: ["http://localhost:3001", "http://127.0.0.1:5500", "https://hereisdx.github.io/chatapp", "https://hereisdx.github.io"],
     mongoString: "mongodb+srv://root:divyanshu@hereisdx-khs4b.mongodb.net/chatapp?retryWrites=true&w=majority",
 };
 
